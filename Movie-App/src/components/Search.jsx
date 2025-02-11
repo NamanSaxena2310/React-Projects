cb64ebf3ef36b1af2c65344b490054cadaecef4d
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-02-10 21:25:45 -0500

    Header and Seacrh Section

---
+++ Movie-App/src/components/Search.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Search = ({searchTerm,setSearchterm}) => {
+  return (
+    <div className='search' >
+      <div>
+        <img src='search.png' alt='Search'/>
+      </div>
+
+      <input type='text'
+        placeholder='Search through thousands of Movies'
+        value={searchTerm}
+        onChange={(e)=>{
+          setSearchterm(e.target.value)
+        }}
+      />
+    </div>
+  )
+}
+
+export default Search
