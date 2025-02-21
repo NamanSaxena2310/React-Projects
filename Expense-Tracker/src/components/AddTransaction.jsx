4813cd60ebe73bb6d0fea3d2a87610e41f5b5a7b
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-02-21 00:08:15 -0500

    Added delte transaction and delte transaction functionalities

--- Expense-Tracker/src/components/AddTransaction.jsx
+++ Expense-Tracker/src/components/AddTransaction.jsx
@@ -1,12 +1,27 @@
-import React,{useState} from 'react'
+import React,{useContext, useState} from 'react'
+import { GlobalContext } from '../context/GlobalState'
 
 function AddTransaction() {
   const [text,setText] = useState('')
   const [amount,setAmount] = useState(0)
+
+  const {addTransactions} = useContext(GlobalContext)
+
+  const onSubmit = e =>{
+    e.preventDefault();
+
+    const newTransaction = {
+      id: Math.floor(Math.random() * 100000000),
+      text,
+      amount: +amount
+    }
+
+    addTransactions(newTransaction)
+  }
   return (
     <>
       <h3>Add new transaction</h3>
-      <form >
+      <form onSubmit={onSubmit} >
         <div className="form-control">
           <label htmlFor="text">Text</label>
           <input type="text" value={text} onChange={(e)=>{
