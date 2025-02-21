1a711bff7ff7fd3316bd045e4b6bb09575670735
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-02-20 23:38:19 -0500

    IncomeExpenses calculation and Transactioon List logic Implemented

--- Expense-Tracker/src/components/Balance.jsx
+++ Expense-Tracker/src/components/Balance.jsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { GlobalContext } from '../context/GlobalState'
+import Transaction from './Transaction'
 
 const Balance = () => {
+  const {transactions} = useContext(GlobalContext)
+
+  const amounts = transactions.map((transaction)=>{
+    return transaction.amount
+  })
+
+  const total = amounts.reduce((acc, amount)=> (acc += amount),0 ).toFixed(2)
+
   return (
     <>
       <h4 className='text-sm'>Your Balance</h4>  
-      <h1 >$0.00</h1>
+      <h1 >${total}</h1>
     </>
   )
 }
