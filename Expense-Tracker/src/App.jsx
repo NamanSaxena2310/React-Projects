604fea61d05102d9c6f6b868044d8d23afcfc38e
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-02-20 23:18:07 -0500

    Created Global Context API for Transactions

--- Expense-Tracker/src/App.jsx
+++ Expense-Tracker/src/App.jsx
@@ -6,13 +6,14 @@ import Balance from './components/Balance'
 import IncomeExpenses from './components/IncomeExpenses'
 import TransactionList from './components/TransactionList'
 import AddTransaction from './components/AddTransaction'
+import { GlobalProvider } from './context/GlobalState'
 
 
 
 function App() {
 
   return (
-    <div>
+    <GlobalProvider>
     <Header/>
     <div className='container'>
       <Balance/>
@@ -20,7 +21,7 @@ function App() {
       <TransactionList/>
       <AddTransaction/>
     </div>
-    </div>
+    </GlobalProvider>
   )
 }
 
