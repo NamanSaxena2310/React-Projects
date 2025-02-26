0347558174ace40bb87e685cede574ef2ab65d19
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-02-25 19:43:01 -0500

    Header Created

---
+++ Brainer/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='p-8 bg-gradient-to-r from-slate-900 to-slate-400  drop-shadow-xl'>
+        <h1 className='text-4xl  text-center text-amber-50'>Welcome to Brainer Game</h1>
+    </div>
+  )
+}
+
+export default Header
