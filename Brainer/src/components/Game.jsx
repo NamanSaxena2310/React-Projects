59b77d7cb9dc6f0a9ee9c7b4968e9cee8f12fe07
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-02-25 23:03:59 -0500

    Game grid

--- Brainer/src/components/Game.jsx
+++ Brainer/src/components/Game.jsx
@@ -1,11 +1,32 @@
-import React from 'react'
+import { useState } from "react";
 
 function Game() {
+  const [grid, setGrid] = useState(5);
+
+  const cells = [];
+  for (let i = 0; i < grid * grid; i++) {
+    cells.push(i + 1);
+  }
+
   return (
-    <div>
-      This is Game Component
-    </div>
-  )
+    <>
+    
+
+      <div className="flex justify-center align-middle border h-screen p-20">
+      <div
+        className="grid gap-4 "
+        style={{ gridTemplateColumns: `repeat(${grid}, minmax(50px, 100px))` }}
+      >
+        {cells.map((cell) => (
+          <div key={cell} className="border border-amber-50 p-5 bg-blue-500 text-white">
+            {cell}
+          </div>
+        ))}
+      </div>
+      </div>
+     
+    </>
+  );
 }
 
-export default Game
+export default Game;
