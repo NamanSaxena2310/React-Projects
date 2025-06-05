1b07e2baa49da8e651a9819cd8c8d1ef514333b7
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-06-05 16:47:53 -0400

    added fetch balance api

--- paytm/backend/controllers/accountController.js
+++ paytm/backend/controllers/accountController.js
@@ -1,13 +1,35 @@
 //  fetch Balance 
 
+const { success } = require("zod/v4")
+const { Account } = require("../models/Account")
+
 const fetchBalance = async (req,res,next) => {
-  
+  try {
+    const userId = req.userId
+    if (!userId) {
+      return res.json({
+         success:false,
+        message:"Unauthorized"
+      })
+    }
+    const account = await Account.findOne({userId})
+    res.json({
+      success:true,
+      data:account.balance
+    })
+  } catch (error) {
+    console.log(error)
+    res.json({
+      success:false,
+      message:error.message
+    })
+  }
 }
 
 // Transer Money
 
 const transferMoney = async (req,res,next) => {
-  
+      
 }
 
 
