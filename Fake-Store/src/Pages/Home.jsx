ce177eb9e8d3936e26c8b179aafb43792784ece5
NamanSaxena2310git config --global user.name NamanSaxena2310git config --global user.name NamanSaxena2310 <namansaxena69@gmail.com>
2025-08-19 19:30:54 -0400

    Products Page MVP

--- Fake-Store/src/Pages/Home.jsx
+++ Fake-Store/src/Pages/Home.jsx
@@ -1,53 +1,54 @@
-import React from 'react'
-import HeroSlider from '../Components/HeroSlider'
-import Headings from '../Components/Headings'
-import CategorySection from '../Components/CategorySection'
-import ProductCard from '../Components/ProductCard'
+import React from "react";
+import HeroSlider from "../Components/HeroSlider";
+import Headings from "../Components/Headings";
+import CategorySection from "../Components/CategorySection";
+import ProductCard from "../Components/ProductCard";
 
 const Home = () => {
   return (
     <div>
       <div>
-        <HeroSlider/>
+        <HeroSlider />
       </div>
 
-      <div className='text-center sm:my-10 my-5 '>
-        <Headings text1={'Explore Our'} text2={'Products'} lineBreak={false}/>
+      <div className="text-center sm:my-10 my-5 ">
+        <Headings text1={"Explore Our"} text2={"Products"} lineBreak={false} />
       </div>
 
       <div>
-        <CategorySection/>
+        <CategorySection />
       </div>
 
-      <div className='text-center sm:my-10 my-5 '><Headings text1={"Our"} text2={"Bestsellers"} lineBreak={false}/></div>
+      <div className="text-center sm:my-10 my-5 ">
+        <Headings text1={"Our"} text2={"Bestsellers"} lineBreak={false} />
+      </div>
 
       {/* Best Seller Section  */}
-    <div className="max-w-7xl  mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-</div>
+      <div className="max-w-7xl  mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
 
-{/* Limited timeOffer section */}
+      {/* Limited timeOffer section */}
 
-<div className="max-w-7xl  mx-auto text-center sm:my-10 my-5 ">
-  <Headings text1={'Flyer'} text2={'Deals'} lineBreak={false}/>
-</div>
+      <div className="max-w-7xl  mx-auto text-center sm:my-10 my-5 ">
+        <Headings text1={"Flyer"} text2={"Deals"} lineBreak={false} />
+      </div>
 
-<div className="max-w-7xl  mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-  <ProductCard />
-</div>
-
+      <div className="max-w-7xl  mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-6 px-4">
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+        <ProductCard />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- Fake-Store/src/Pages/Products.jsx
+++ Fake-Store/src/Pages/Products.jsx
@@ -1,8 +1,60 @@
 import React from 'react'
+import Headings from '../Components/Headings'
+import ProductCard from '../Components/ProductCard'
 
 const Products = () => {
   return (
-    <div>Products</div>
+    <div className='max-w-7xl mx-auto text-primary lg:flex md:flex gap-20 py-10 px-4'>
+      {/* filters section */}
+      <div className=' flex flex-col  gap-5 min-w-50 py-5 '>
+        <h3 className='text-xl font-bold'>Filters</h3>
+        {/* Categories */}
+        <div className='border border-gray-300 p-3'>
+            <h3>Categories</h3>
+            <div className="">
+              <p className='flex gap-2'><input type='checkbox'></input>Men</p>
+              <p className='flex gap-2'><input type='checkbox'></input>Women</p>
+              <p className='flex gap-2'><input type='checkbox'></input>Kids</p>
+            </div>
+        </div>
+
+         <div className='border border-gray-300 p-3'>
+            <h3>Categories</h3>
+            <div className="">
+              <p className='flex gap-2'><input type='checkbox'></input>Men</p>
+              <p className='flex gap-2'><input type='checkbox'></input>Women</p>
+              <p className='flex gap-2'><input type='checkbox'></input>Kids</p>
+            </div>
+        </div>
+      </div>
+
+      {/* Products Section */}
+      <div className='flex flex-col gap-y-10 ' >
+        <div className='text-center md:text-left'>
+            <Headings text1={"All"} text2={"Collections"} lineBreak={false}/>
+        </div>
+        
+        {/* Products */}
+        <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6 '>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+          <ProductCard/>
+        </div>
+      </div>
+    </div>
   )
 }
 
